e62df32395604f7d8ee15bb0c9995762677ffe57
ysanchezhaye <ysanchezhaye@gmail.com>
2022-07-14 18:19:54 -0300

    clase 34 - hasta punto 44

--- routes/rutas.js
+++ routes/rutas.js
@@ -37,7 +37,7 @@ const {
     loginGET
 }= require('../controllers/back.ctrl')
 
-router.get('/admin',adminGET )
+router.get('/admin',adminGET ) 
 
 router.get('/agregar-producto', agregarProductoGET)
 router.post('/agregar-producto', agregarProductoPOST)
